28614cda8af4f96dcf09efe0815d323b5c24ee37
Sonia Goncalves <i_code@fastmail.org>
2021-07-27 23:50:44 +0100

    Started to implement the main() and tokenize() functions.

--- interpreter.py
+++ interpreter.py
@@ -15,24 +15,58 @@
 
 def main():
 
-    print("Enter 'Q' to quit.")
+    print("Calculator\nEnter the key 'Q' to exit the program.")
     userInput = input(">>> ")
+    while userInput != 'Q':
+            
+        tokens = tokenize(userInput)
         
-    sign = 'Q'
-    while  sign.upper() != 'Q'
+        addition = 0
+        #for i in range(len(tokens)):
+        if tokens[0] == '+':
+            for j in range (1, len(tokens)):
+                addition += int(tokens[j])
+        else:
+            print("Other operators are still not defined!")
+
+        print (addition)
+
         userInput = input(">>> ")
 
-    
-    tokens = tokenize(userInput)
-
-    parse(tokens)
+        #parse(tokens)
 
 
+# What this function does (remember, not how it does!)
 def tokenize(userInput):
+    '''(str) -> list of strings (tokens)
 
+    Return a list of valid tokens from the string userInput.
 
+    >>> + 1 2
+    3
+    >>> + 10 20
+    30
+    >>> + 11 21 31
+    63
+    '''
 
-def parse(tokens):
+    tokens = []
+    charList = userInput.split()
+
+    pos = 0
+    while pos < len(charList):
+        if charList[pos] == '+':
+            tokens.append(charList[pos])
+        elif charList[pos].isdigit():
+            tokens.append(charList[pos])
+        else:
+            print("Unexpected token!") 
+        pos += 1
+
+    return tokens
+
+    
+#def parse(tokens):
     
 
 main()
