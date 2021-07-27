cb84c838ce83af2f61eb23d6d3e95690ec4d8fb0
Sonia Goncalves <i_code@fastmail.org>
2021-07-28 00:55:09 +0100

    Corrected the docstring of the tokenize() function.

--- interpreter.py
+++ interpreter.py
@@ -1,10 +1,8 @@
 # Sonia da Trindade
 # 24 07 21
 
-
 # This is an interpreter.
 # This is the main file of this program.
-
 
 # Tokenizer
 # Parser
@@ -17,36 +15,7 @@
 
 
 def main():
-
-    print("Calculator\nEnter the key 'Q' to exit the program.")
-    userInput = input(">>> ")
-    
-    while userInput.upper() != 'Q':
-            
-        tokens = tokenize(userInput)
-        
-        addition = 0
-        if tokens[0] == '+':
-            for i in range (1, len(tokens)):
-                addition += int(tokens[i])
-        else:
-            print("Other operators are still not defined!")
-
-        print (addition)
-
-        userInput = input(">>> ")
-
-        #parse(tokens)
-
-    print("The program has exited.")
-    
-
-# What this function does (remember, not how it does!)
-def tokenize(userInput):
-    '''(str) -> list of strings (tokens)
-
-    Return a list of valid tokens from the string userInput.
-
+    '''
     >>> + 1 2
     3
     >>> + 10 20
@@ -55,9 +24,51 @@ def tokenize(userInput):
     63
     '''
 
+    print("Calculator\nEnter the key 'Q' to exit the program.")
+    
+    userInput = input(">>> ")
+    while userInput.upper() != 'Q':
+            
+        tokens = tokenize(userInput)
+        #print(tokens)
+
+        if len(tokens) != 0:
+            addition = 0
+            if tokens[0] == '+':
+                for i in range (1, len(tokens)):
+                    addition += int(tokens[i])
+            else:
+                print("Other operators are still not defined!")
+
+            print(addition)
+        else:
+            print("Something went wrong!")
+
+        userInput = input(">>> ")
+
+        #parse(tokens)
+
+    print("The program has exited.")
+    
+
+# Comment here what this function does (remember, not how it does!)
+def tokenize(userInput):
+    '''(str) -> list of strings (tokens)
+
+    Return a list of valid tokens from the string userInput.
+
+    >>> tokenize('+ 1 2')
+    ['+', '1', '2']
+    >>> tokenize('+ 10 20') 
+    ['+', '10', '20']
+    >>> tokenize('+ 11 21 31')
+    ['+', '11', '21', '31']
+    '''
+
     tokens = []
     charList = userInput.split()
 
+    # I think it's a good idea to comment every substancial block of code.
     pos = 0
     while pos < len(charList):
         if charList[pos] == '+':
@@ -75,4 +86,3 @@ def tokenize(userInput):
     
 
 main()
-
