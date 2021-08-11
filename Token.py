0f01702de1c92dcef5e74bcd16573605f05aa463
Sonia Goncalves <i_code@fastmail.org>
2021-08-11 03:33:28 +0100

    Created a file, and started the impl. of class Token.

---
+++ Token.py
@@ -0,0 +1,15 @@
+# Sonia
+# 11 08 21
+
+
+# Token is a component resultant from breaking the input.
+# Token can be defined as an object with several properties.
+
+
+class Token:
+
+    def __init__(self, type, value, index):
+
+        self.type = type
+        self.value = value
+        self.index = index
