3712609c8a2771b46086e00afddbb11218b3dc6d
Sonia Goncalves <i_code@fastmail.org>
2021-08-11 03:24:16 +0100

    Created a file for the impl. of the tokenizer. Added notes.

---
+++ Tokenizer.py
@@ -0,0 +1,15 @@
+# Sonia
+# 11 08 21
+
+
+# Lexical analysis is the process of breaking the input string into tokens.
+# It's the part of the interpreter that turns the input of characters into a stream of tokens.
+# Other terms for LA: Lexer, scanner, tokenizer.
+
+
+# read the input of characters.
+
+# convert it into a stream of tokens.
+
+# return the tokens to feed the parser :)
+
