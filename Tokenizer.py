a6bd20c799a1876d022d7800d946966d882f79a8
Sonia Goncalves <i_code@fastmail.org>
2021-08-21 18:56:59 +0100

    Class Tokenizer. Incomplete and buggy :)

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,15 +1,110 @@
-# Sonia
-# 11 08 21
+# Sonia da Trindade
+# 19 08 21
 
 
-# Lexical analysis is the process of breaking the input string into tokens.
-# It's the part of the interpreter that turns the input of characters into a stream of tokens.
-# Other terms for LA: Lexer, scanner, tokenizer.
+import token
+
+class Tokenizer:
+    '''A tokenizer.'''
+
+    def __init__(self, userInput):
+        '''(Tokenizer, str) -> NoneType'''
+
+        self._input = userInput # stream of characteres (str)
+        self._pos = 0
+        self._current_char = self._input[self._pos]
+        self._output = [] # stream of lexemes/tokens (list)
 
 
-# read the input of characters.
+    def __str__(self):
+        '''(Tokenizer) -> str
 
-# convert it into a stream of tokens.
+        Return a string representation of the tokenizer.
 
-# return the tokens to feed the parser :)
+        >>> tk = Tokenizer('+ 1 2')
+        >>> str(tk)
+        '(+ 1 2) -> Tokenizer -> ['+', '1', '2']'
+        '''
+        
+        return '(' + self._input + ') -> Tokenizer -> ' + self._output
+    
+    
+    def advanceOnePosition(self):
+        '''(Tokenizer) -> NoneType '''
+
+        self._pos += 1
+        self._current_char = self._input[self._pos]
+
+
+    # How to write test examples?
+    def isMultidigit(self):
+        '''(Tokenizer) -> int
+
+        Return a multidigit integer (lexeme). 
+        '''
+        
+        result = 0
+
+        while self._current_char.isdigit():
+            result += self._current_char
+            self.advanceOnePosition()
+            
+        return result
+
+
+    # refactor!
+    def prepareDataFromInput(self):
+        '''(Tokenizer) -> Token'''
+
+        while self._pos < len(self._input):
+
+            if self._current_char.isspace():
+                self.advanceOnePosition()
+                continue # this statement returns the control to the beginning of the while loop :)
+            
+            if self._current_char == '+':
+                    tk = token.Token(PLUS, '+', self._pos)
+                    self._output.append(tk)
+                
+            if self._current_char == '-':
+                    tk = token.Token(MINUS, '-', self._pos)
+                    self._output.append(tk)
+                
+##            if self._current_char == '*':
+##                    self._output.append(Token(MULT, '*', self._pos))
+##                
+##            if self._current_char == '/':
+##                    self._output.append(Token(DIV, '/', self._pos))
+
+            if self._current_char.isdigit():
+                    tk = Token(INT, self.IsMultidigit(), self._pos)
+                    self._output.append(tk) # need to come up with a better name...
+                
+            self.advanceOnePosition()
+                                       
+        # another alterantive for invalid token, e.g. '!'
+        # parser verifica de certo modo a ordem dos tokens, eu penso! :)
+
+
+    def getListOfTokens(self):
+        '''(Tokenizer) -> list of Tokens
+
+        Return a list of valid tokens.
+
+        >>> tk = Tokenizer('+ 1 2')
+        >>> tk.getListOfTokens()
+        ['+', '1', '2']
+        '''
+
+        self.prepareDataFromInput()
+        
+        return self._output
+
+
+if __name__ == '__main__':
+
+    tkz = Tokenizer('+ 1 2')
+    listOfTokens = tkz.getListOfTokens()
+    print(listOfTokens)
+
 
