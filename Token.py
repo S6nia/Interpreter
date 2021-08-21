f3ca82975053e3c163d4ae02ce703f4797af9e0d
Sonia Goncalves <i_code@fastmail.org>
2021-08-21 18:43:43 +0100

    Class Token.

--- Token.py
+++ Token.py
@@ -1,15 +1,115 @@
 # Sonia
-# 11 08 21
+# 18 08 21
 
 
-# Token is a component resultant from breaking the input.
-# Token can be defined as an object with several properties.
+# Tomorrow:
+# main +-
+# tokenizer +-
+# token class done
 
+# parser
+# node class
+
+# Evaluator
+# Writing
+
+
+# Need to investigate enumerations
+INT, PLUS, MINUS, MULT, DIV, LPAREN, RPARENT = 'INT', 'PLUS', 'MINUS', 'MULT', 'DIV', 'LPAREN', 'RPARENT'
 
 class Token:
+    '''A token.'''
 
     def __init__(self, type, value, index):
+        '''(Token, str, str, int) -> Nonetype
 
-        self.type = type
-        self.value = value
-        self.index = index
+        >>> token = Token('INT', 2, 0)
+        >>> token.getType()
+        'INT'
+        >>> token.getValue()
+        2
+        >>> token.getIndex()
+        0
+        '''
+
+        self._type = type
+        self._value = value
+        self._index = index
+        
+
+    def __str__(self):
+        '''(Token) -> str
+
+        Return a string representation of the token.
+
+        >>> token = Token('INT', '2', 0)
+        >>> str(token)
+        'Token {type: INT, value: 2, index: 0}'
+        '''
+
+        return 'Token {type: ' + self._type + ', ' + 'value: ' + self._value \
+               + ', index: ' + str(self._index) + '}'
+
+
+    def getType(self):
+        '''(Token) -> str
+
+        Return the type of the token (INT, PLUS, MINUS, MULT, DIV, LPAREN, RPAREN).
+
+        >>> token = Token('INT', 2, 0)
+        >>> token.getType()
+        'INT'
+        '''
+        
+        return self._type
+
+
+    def getValue(self):
+        '''(Token) -> str
+
+        Return the value of the token.
+
+        >>> token = Token('INT', 2, 0)
+        >>> token.getValue()
+        2
+        '''
+        return self._value
+
+
+    def getIndex(self):
+        '''(Token) -> int
+
+        Return the index of the position of the token in the input.
+
+        >>> token = Token('INT', 2, 0)
+        >>> token.getIndex()
+        0
+        '''
+        
+        return self._index
+
+
+# The following segment is executed if this file is to be run.
+if __name__ == '__main__':
+
+    # Testing automatically using doctest module.
+    import doctest
+    doctest.testmod()
+
+    # Creating and testing some examples.
+    tk1 = Token('INT', '2', 0)
+    tk2 = Token('PLUS', '+', 1)
+
+    print(tk1)
+    print(tk2)
+
+    print(tk1._type)
+    print(tk2._type)
+    print(tk1.getValue())
+    print(tk2.getValue())
+    print(tk1.getType())
+    print(tk2.getType())
+    print(tk1.getIndex())
+    print(tk2.getIndex())
+    
+  
