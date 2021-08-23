237369eb376c4c59d9eb6b46263507af40fb34ac
Sonia Goncalves <i_code@fastmail.org>
2021-08-23 23:22:10 +0100

    Review: Comments, corrections n improvements. Clean up.

--- Tokenizer.py
+++ Tokenizer.py
@@ -1,105 +1,108 @@
-# Sonia da Trindade
-# 19 08 21
+# Name: Sonia Gonçalves
+# Program: IT MSc
+# ST ID: 
+# Date: 19 08 21
 
+
+##
+# This module defines the Tokenizer class.
+#
 
 import token
 
 class Tokenizer:
     '''A tokenizer.'''
 
-    # Constructor of the class.
     def __init__(self, userInput):
         '''(Tokenizer, str) -> NoneType'''
 
-        self._input = userInput # stream of characteres (str)
+        self._input = userInput # stream of chars (str)
         self._pos = 0
-        self._current_char = self._input[self._pos]
+        self._currentChar = self._input[self._pos]
         self._output = [] # stream of lexemes/tokens (list)
 
 
-    # String representation of the class.
     def __str__(self):
         '''(Tokenizer) -> str
 
         Return a string representation of the tokenizer.
 
-        >>> tk = Tokenizer('+ 1 2')
-        >>> str(tk)
+        >>> tkz = Tokenizer('+ 1 2')
+        >>> print(tkz)
         '(+ 1 2) -> Tokenizer -> ['+', '1', '2']'
         '''
         
         return '(' + self._input + ') -> Tokenizer -> ' + self._output
-    
 
-    # Helper method that increments the position,
-    # and assign the next char to the current one.
-    def advanceOnePosition(self):
-        '''(Tokenizer) -> NoneType '''
+
+    def advanceByOne(self):
+        '''(Tokenizer) -> NoneType'''
 
         self._pos += 1
-        self._current_char = self._input[self._pos]
+        self._currentChar = self._input[self._pos]
+    
 
-
-    # Helper method that checks if the current integer is multidigit.
-    # How to write test examples for this case, w/out 
     def getMultidigitInteger(self):
         '''(Tokenizer) -> int
 
         Return a multidigit integer (lexeme). 
         '''
         
-        result = 0
+        result = ''
 
-        while self._current_char.isdigit():
-            result += self._current_char
-            self.advanceOnePosition()
+        while self._currentChar.isdigit():
+            result += self._currentChar
+            self.advanceByOne()
             
         return result
 
-
-    # This method cleans the input from whitespaces, checks the expected chars,
-    # and then creates tokens.
+    
     def prepareDataFromInput(self):
         '''(Tokenizer) -> NoneType'''
 
         while self._pos < len(self._input):
 
-            if self._current_char.isspace():
-                self.advanceOnePosition()
-                continue # this statement returns the control to the beginning of the while loop :)
+            if self._currentChar.isspace():
+                self.advanceByOne()
+                continue # this statement returns the control to the beginning of the while loop :) 
             
-            if self._current_char == '+':
+            else:
+
+                if self._current_char == '+':
                     tk = token.Token(PLUS, '+', self._pos)
                     self._output.append(tk)
                 
-            if self._current_char == '-':
+                elif self._current_char == '-':
                     tk = token.Token(MINUS, '-', self._pos)
                     self._output.append(tk)
-                
-##            if self._current_char == '*':
-##                    self._output.append(Token(MULT, '*', self._pos))
-##                
-##            if self._current_char == '/':
-##                    self._output.append(Token(DIV, '/', self._pos))
 
-            if self._current_char.isdigit():
-                    tk = Token(INT, self.IsMultidigit(), self._pos)
+                elif self._current_char == '*':
+                    tk = token.Token(MULT, '*', self._pos)
+                    self._output.append(tk)
+
+                elif self._current_char == '/':
+                    tk = token.Token(DIV, '/', self._pos)
+                    self._output.append(tk)
+
+                elif self._current_char.isdigit():
+                    tk = token.Token(INT, self.getMultidigitInteger(), self._pos)
                     self._output.append(tk) # need to come up with a better name...
+
+                else:
+                    print("Invalid character!") # needs to be improved based on the info attached to the char.
+                                                # to give more info to the user.
                 
-            self.advanceOnePosition()
-                                       
-        # another alterantive for invalid token, e.g. '!'
-        # parser verifica de certo modo a ordem dos tokens, eu penso! :)
+            self.advanceByOne()
 
 
-    # This method gets the list of tokens and returns it.
     def getListOfTokens(self):
         '''(Tokenizer) -> list of Tokens
 
         Return a list of valid tokens.
 
-        >>> tk = Tokenizer('+ 1 2')
-        >>> tk.getListOfTokens()
+        >>> tkz = Tokenizer('+ 1 2')
+        >>> tokens = tkz.getListOfTokens()
+        >>> print(tokens)
         ['+', '1', '2']
         '''
 
@@ -110,8 +113,10 @@ class Tokenizer:
 
 if __name__ == '__main__':
 
-    tkz = Tokenizer('+ 1 2')
-    listOfTokens = tkz.getListOfTokens()
-    print(listOfTokens)
+    #import doctest
+    #doctest.testmod()
 
+    tkz = Tokenizer('+ 1 2')
+    Tokens = tkz.getListOfTokens()
+    print(Tokens)
 
