300793fa36e2854107260724721d9199a00f77d9
Sonia Goncalves <i_code@fastmail.org>
2021-08-29 23:43:11 +0100

    Defined MyEvaluator class.

--- myEvaluator.py
+++ myEvaluator.py
@@ -5,8 +5,42 @@
 
 
 ##
-# This module defines the Evaluator class.
+# This module defines the MyEvaluator class.
 #
 
-class Evaluator:
+class MyEvaluator:
     '''An evaluator.'''
+
+    def __init__(self, op, opd1, opd2, order):
+
+        self._operator = op
+        self._operand1 = opd1
+        self._operand2 = opd2
+        self._precedence = order
+
+        self._result = 0
+
+        
+    #def __str__(self):
+        
+
+    def _performOperation(self):
+
+        if self._operator == '+':
+            self._result = self._operand1 + self._operand2
+
+        elif self._operator == '-':
+            self._result = self._operand1 - self._operand2
+
+        elif  self._operator == '*':
+            self._result = self._operand1 * self._operand2
+
+        else:
+            self._result = self._operand1 / self._operand2
+
+
+    def getResult(self):
+
+        return self._result
+
+        
