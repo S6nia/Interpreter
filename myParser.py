c490b71e82e0876ddc5bde78f63822d131e68575
Sonia Goncalves <i_code@fastmail.org>
2021-08-30 02:34:58 +0100

    Defining MyParser class...

--- myParser.py
+++ myParser.py
@@ -8,8 +8,107 @@
 
 
 ##
-# This module defines the Parser class.
+# This module defines the MyParser class.
 #
 
-class Parser:
+from myToken import MyToken
+
+class MyParser:
     '''A parser.'''
+
+    def __init__(self, tokens):
+
+        self._tokens = tokens # list of obj of type token
+        #self._firstToken = self._tokens[0].getValue()
+        #self._node = node
+        self._result = 0
+
+
+    #def __str__():
+    
+
+    # Following syntax diagrams/grammar <-- check this
+    def _getExpression(self):
+
+        #value = self._getTerm()
+        #result = 0
+
+        done = False
+        while not done and len(self._tokens) > 0:
+
+            currenToken = self._tokens[0] # token to be analised, current
+            currenTokenValue = currenToken.getValue()
+            print(currenTokenValue)
+            
+            if  currenTokenValue == '+' or  currenTokenValue == '-':
+                test = self._tokens.pop(0)
+                print(test)
+                value1 = self._getTerm()
+                value2 = self._getTerm()
+
+                if currenTokenValue == '+':
+                    result = value1 + value2
+
+                else:
+                    result = value1 - value2
+
+            else:
+                done = True
+
+        return result
+        
+
+    def _getTerm(self):
+
+        result = self._getFactor()
+
+        done = False
+        while not done and len(self._tokens) > 0:
+
+            currenToken = self._tokens[0] # token to be analised, current
+            currenTokenValue = currenToken.getValue()
+            if  currenTokenValue == '*' or  currenTokenValue == '/':
+                self._tokens.pop(0)
+                value1 = self._getFactor()
+                value2 = self._getFactor()
+
+                if currenTokenValue == '*':
+                    result = value1 * value2
+
+                else:
+                    result = value1 / value2
+
+            else:
+                done = True
+
+        return result
+        
+
+    def _getFactor(self):
+
+        token = self._tokens.pop(0) # error message subscriptable! lol
+        print(token)
+        tokenValue = token.getValue()
+        print(tokenValue)
+
+        if tokenValue.isdigit():
+            return tokenValue
+
+
+    def getResult(self):
+
+        self._result = self._getExpression()
+
+        return self._result
+
+
+if __name__ == '__main__':
+
+    from myTokenizer import MyTokenizer
+
+    tkz = MyTokenizer('+ 1 2')
+    tokens = tkz.getListOfTokens()
+    psr = MyParser(tokens)
+    result = psr.getResult()
+    print(result)
+    
