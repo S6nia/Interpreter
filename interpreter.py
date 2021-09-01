78b8c4354694fa36da1eba03b86f2f955b19f47a
Sonia Goncalves <i_code@fastmail.org>
2021-09-01 01:23:50 +0100

    Adaptation to new developments on its dependencies.

--- interpreter.py
+++ interpreter.py
@@ -13,7 +13,7 @@
 # Parser
 # Evaluator
 #
-# Documentation: Comments, docstring, preconditions
+# Documentation: Comments, docstring, preconditions, public interfaces (classes)
 # Validations, and error handling
 # Testing (doctest, unittest, assertions)
 
@@ -22,10 +22,9 @@
 # This module defines the main function of the program: Interpreter Abacus :)
 #
 
-import tokenizer
-# TBD:
-#import parser   
-#import evaluator
+from myTokenizer import MyTokenizer
+from myParser import MyParser
+#from myEvaluator import MyEvaluator
 
 def main():
     '''
@@ -44,12 +43,14 @@ def main():
     userInput = input(">>> ")
     while userInput.upper() != 'Q':
             
-        tk = tokenizer.Tokenizer(userInput)
+        tk = MyTokenizer(userInput)
         tokens = tk.getListOfTokens()
-        psr = parser.Parser(tokens)
-        expression = psr.getExpression()
-        eva = evaluator.Evaluator(expression)
-        result = eva.getResult()
+        psr = MyParser(tokens)
+        #expression = psr.getExpression()
+        #eva = evaluator.Evaluator(expression)
+        #result = eva.getResult()
+
+        result = psr.getResult()
 
         print(result)
 
@@ -59,5 +60,6 @@ def main():
     
 
 if __name__ == '__main__':
+
     main()
 
