f3db19d4d04e65d3be9ee9722101f4dfc0c12f13
Sonia Goncalves <i_code@fastmail.org>
2021-09-03 23:53:22 +0100

    Impl. Node class.

--- myNode.py
+++ myNode.py
@@ -5,8 +5,96 @@
 
 
 ##
-# This module defines the Node class.
+# This module defines the MyNode class.
 #
 
-class Node:
+
+#INT, PLUS, MINUS, MULT, DIV = 'INT', 'PLUS', 'MINUS', 'MULT', 'DIV'
+
+# I still don't know what to do with this, but I'am having this intuition...
+##ROOT, PARENT, CHILD, LEAF, RLEAF, LNODE, RNODE, DEPTH  = (
+##    'ROOT', 'PARENT', 'CHILD', 'LEAF', 'RLEAF', 'LNODE', 'RNODE', 'DEPTH'
+##)
+
+ROOT, LNODE, RNODE, LEAF, RLEAF = 'ROOT', 'LNODE', 'RNODE', 'LEAF', 'RLEAF'
+
+class MyNode:
     '''A node.'''
+
+    def __init__(self, desc, value, pos):
+        '''(MyNode, str, str, int) -> Nonetype
+
+        >>> node = MyNode('ROOT', '+' , 2)
+        >>> node.getType()
+        'ROOT'
+        >>> node.getValue()
+        '+'
+        >>> node.getPosition()
+        2
+        '''
+
+        self._type = desc
+        self._value = value
+        self._pos = pos
+
+
+    def __str__(self):
+        '''(MyNode) -> str
+
+        Return a string representation of the node.
+
+        >>> node = MyNode('ROOT', '+', 2)
+        >>> str(node)
+        'Node {type: ROOT, value: +, position: 2}'
+        '''
+
+        return 'Node {type: ' + self._type + ', ' + 'value: ' + self._value \
+               + ', position: ' + str(self._pos) + '}'
+
+
+    def getType(self):
+        '''(MyNode) -> str
+
+        Return the type of the node.
+
+        >>> node = MyNode('ROOT', '+', 2)
+        >>> node.getType()
+        'ROOT'
+        '''
+        
+        return self._type
+
+
+    def getValue(self):
+        '''(MyNode) -> str
+
+        Return the value of the node.
+
+        >>> node = MyNode('ROOT', '+', 2)
+        >>> node.getValue()
+        '+'
+        '''
+        return self._value
+
+
+    def getPosition(self):
+        '''(MyNode) -> int
+
+        Return the index of the position of the node in the list of nodes.
+
+        >>> node = MyNode('ROOT', '+', 2)
+        >>> node.getPosition()
+        2
+        '''
+        
+        return self._pos
+     
+
+# The following segment is executed if this file is to be run.
+if __name__ == '__main__':
+
+    # Testing automatically using doctest module.
+    import doctest
+    doctest.testmod()
+
+
