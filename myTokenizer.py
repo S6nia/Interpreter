20be397a3dfc5afcb37408efc1a8acfef442b52f
Sonia Goncalves <i_code@fastmail.org>
2021-09-09 00:53:55 +0100

    Review: Corrections, improvements, and clean up.

--- myTokenizer.py
+++ myTokenizer.py
@@ -17,13 +17,14 @@ INT, PLUS, MINUS, MULT, DIV, LPARENT, RPARENT = (
 class MyTokenizer:
     '''A tokenizer.'''
 
+    # Docstring?
     def __init__(self, userInput):
         '''(MyTokenizer, str) -> NoneType'''
 
-        self._input = userInput # stream of chars (str)
+        self._input = userInput # Input: stream of chars (str)
         self._pos = 0
         self._currentChar = self._input[self._pos]
-        self._output = [] # stream of lexemes/tokens (list)
+        self._output = [] # Output: stream of lexemes/tokens (list of str)
 
 
     def __str__(self):
@@ -32,8 +33,9 @@ class MyTokenizer:
         Return a string representation of the tokenizer.
 
         >>> tkz = MyTokenizer('+ 1 2')
+        >>> tokens = tkz.getListOfTokens()
         >>> print(tkz)
-        '(+ 1 2) -> Tokenizer -> ['+', '1', '2']'
+        '+ 1 2' -> Tokenizer -> ['+', '1', '2']
         '''
 
         lst = []
@@ -41,12 +43,12 @@ class MyTokenizer:
             lst.append(tk.getValue())
         
         return '\'' + self._input + '\' -> Tokenizer -> ' + str(lst)
-
     
-    def _prepareDataFromInput(self):
+
+    # Docstring?
+    def _creaTokens(self):
         '''(Tokenizer) -> NoneType'''
 
-        #multiDigit = ''
         for self._pos in range(len(self._input)):
     
             self._currentChar = self._input[self._pos]
@@ -81,24 +83,20 @@ class MyTokenizer:
                     self._output.append(tk)
 
                 elif self._currentChar.isdigit():
-                    #tk = MyToken(INT, self._currentChar, self._pos)
-                    #self._output.append(tk)
 
                     if not self._input[self._pos - 1].isdigit():
-                        #multiDigit = self._currentChar
                         tk = MyToken(INT, self._currentChar, self._pos)
                         self._output.append(tk)
+                        
                     else:
-                        #multiDigit += self._currentChar
-                        #tk = MyToken(INT, multiDigit, self._pos)
-                        #self._output.append(tk)
-                        #self._output[-1]._value += self._currentChar
                         value = self._output[-1].getValue() + self._currentChar
                         self._output[-1].setValue(value)
 
                 else:
-                    print("Invalid character!") # needs to be improved based on the info attached to the char.
-                                                # to give more info to the user.
+                    # needs to be improved based on the info attached to the char.
+                    # to give more info to the user.
+                    print("Invalid character in the position: ", self._pos)
+                                                
 
 
     def getListOfTokens(self):
@@ -106,24 +104,26 @@ class MyTokenizer:
 
         Return a list of valid tokens.
 
-        >>> tkz = Tokenizer('+ 1 2')
+        >>> tkz = MyTokenizer('+ 1 2')
         >>> tokens = tkz.getListOfTokens()
-        >>> ... ... ... ... ... ... ... ...
-        >>> print(list((tokens[0].getValue()), tokens[1].getValue()), tokens[2].getValue())))
-        ['+', '1', '2']
+        >>> print(tkz)
+        '+ 1 2' -> Tokenizer -> ['+', '1', '2']
         '''
 
-        self._prepareDataFromInput()
+        self._creaTokens()
         
         return self._output
 
 
+# The following segment is executed if this file is to be run.
 if __name__ == '__main__':
 
-    #import doctest
-    #doctest.testmod()
+    # Testing automatically using doctest module.
+    import doctest
+    doctest.testmod()
 
-    # Creating some examples. Testing whitespaces.
+    # Creating some examples:
+    # Testing whitespaces
     #tkz = MyTokenizer('+ 1 2')
     #tkz = MyTokenizer('+ 1  2')
     #tkz = MyTokenizer(' + 1 2')
@@ -131,7 +131,7 @@ if __name__ == '__main__':
     #tkz = MyTokenizer('+1 2')
     #tkz = MyTokenizer('+12')
 
-    # Testing with other operators:
+    # Testing with other operators
     #tkz = MyTokenizer('- 1 2')
     #tkz = MyTokenizer('* 1 2')
     #tkz = MyTokenizer('/ 1 2')
@@ -145,20 +145,20 @@ if __name__ == '__main__':
 
     # Testing invalid token
     #tkz = MyTokenizer(' !12')
+    #tkz = MyTokenizer(' 12!')
+
+    # Testing valid token, but invalid grammar(parser job)
+    #tkz = MyTokenizer(' 12+')
 
     # Testing with parentheses
-    tkz = MyTokenizer('+ (* 2 3) (* 2 4)')
-    
-    print(tkz)
-    print()
-    
+    #tkz = MyTokenizer('+ (* 2 3) (* 2 4)')
+
+    # Testing with nested parentheses
+    tkz = MyTokenizer('+ (* 2 3) (* (* 2 1) (* 2 2))')
     tokens = tkz.getListOfTokens()
 
     for tk in tokens:
         print(tk)
-        print(tk.getType())
-        print(tk.getValue())
-        print(tk.getIndex())
         print()
 
     print(tkz)
