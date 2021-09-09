dca01aeea5cf5bfbc466087da1b05fb3ce3d7f0d
Sonia Goncalves <i_code@fastmail.org>
2021-09-09 19:23:23 +0100

    Review: Corrections, improvements, and clean up.

--- myNode.py
+++ myNode.py
@@ -8,16 +8,6 @@
 # This module defines the MyNode class.
 #
 
-
-#INT, PLUS, MINUS, MULT, DIV = 'INT', 'PLUS', 'MINUS', 'MULT', 'DIV'
-
-# I still don't know what to do with this, but I'am having this intuition...
-##ROOT, PARENT, CHILD, LEAF, RLEAF, LNODE, RNODE, DEPTH  = (
-##    'ROOT', 'PARENT', 'CHILD', 'LEAF', 'RLEAF', 'LNODE', 'RNODE', 'DEPTH'
-##)
-
-#ROOT, LNODE, RNODE, LEAF, RLEAF = 'ROOT', 'LNODE', 'RNODE', 'LEAF', 'RLEAF'
-
 OPERATOR, LOPERAND, ROPERAND = 'OPERATOR', 'LOPERAND', 'ROPERAND'
 
 class MyNode:
@@ -26,36 +16,20 @@ class MyNode:
     def __init__(self, operator, leftOpd, rightOpd = None):
         '''(MyNode, str, int, int) -> Nonetype
 
-        >>> node = MyNode('ROOT', '+' , 2)
-        >>> node.getType()
-        'ROOT'
-        >>> node.getValue()
+        Initializes the instance variables of node object, given the operator, leftOpd, rightOpd.
+
+        >>> node = MyNode('+', '1' , '2')
+        >>> node.getOperator()
         '+'
-        >>> node.getPosition()
-        2
+        >>> node.getLeftOperand()
+        '1'
+        >>> node.getRightOperand()
+        '2'
         '''
 
         self._operator = operator
         self._leftOpd = leftOpd
-        self._rightOpd = rightOpd
-        
-
-##        def __init__(self, desc, value, pos):
-##        '''(MyNode, str, str, int) -> Nonetype
-##
-##        >>> node = MyNode('ROOT', '+' , 2)
-##        >>> node.getType()
-##        'ROOT'
-##        >>> node.getValue()
-##        '+'
-##        >>> node.getPosition()
-##        2
-##        '''
-##
-##        self._type = desc
-##        self._value = value
-##        self._pos = pos
-
+        self._rightOpd = rightOpd        
 
 
     def __str__(self):
@@ -63,51 +37,50 @@ class MyNode:
 
         Return a string representation of the node.
 
-        >>> node = MyNode('ROOT', '+', 2)
+        >>> node = MyNode('+', '1', '2')
         >>> str(node)
-        'Node {type: ROOT, value: +, position: 2}'
+        'Node {Operator: +, Left value: 1, Right value: 2}'
         '''
 
-##        return 'Node {type: ' + self._type + ', ' + 'value: ' + self._value \
-##               + ', position: ' + str(self._pos) + '}'
-
-        return 'Node {operator: ' + self._op + ', ' + 'leftValue: ' + str(self._op1) \
-               + ', rightValue: ' + str(self._op2) + '}'
+        # Improve this for operations involving only one number
+        return 'Node {Operator: ' + self._operator + ', ' + 'Left value: ' + str(self._leftOpd) \
+               + ', Right value: ' + str(self._rightOpd) + '}'
 
 
     def getOperator(self):
         '''(MyNode) -> str
 
-        Return the type of the node.
+        Return the value of the node (operator).
 
-        >>> node = MyNode('ROOT', '+', 2)
-        >>> node.getType()
-        'ROOT'
+        >>> node = MyNode('+', '1', '2')
+        >>> node.getOperator()
+        '+'
         '''
         
         return self._operator
 
 
     def getLeftOperand(self):
-        '''(MyNode) -> str
+        '''(MyNode) -> int
 
-        Return the value of the node.
+        Return the value of the left leaf (left operand).
 
-        >>> node = MyNode('ROOT', '+', 2)
-        >>> node.getValue()
-        '+'
+        >>> node = MyNode('+', '1', '2')
+        >>> node.getLeftOperand()
+        '1'
         '''
+        
         return self._leftOpd
 
 
     def getRightOperand(self):
         '''(MyNode) -> int
 
-        Return the index of the position of the node in the list of nodes.
+        Return the value of the right leaf (right operand).
 
-        >>> node = MyNode('ROOT', '+', 2)
-        >>> node.getPosition()
-        2
+        >>> node = MyNode('+', '1', '2')
+        >>> node.getRightOperand()
+        '2'
         '''
         
         return self._rightOpd
@@ -117,25 +90,14 @@ class MyNode:
 if __name__ == '__main__':
 
     # Testing automatically using doctest module.
-##    import doctest
-##    doctest.testmod()
+    import doctest
+    doctest.testmod()
 
     # Creating some examples
-    # Taking as an example: + (* 2 3) (* 3 3)
-##    node1 = MyNode('LEAF', '3', 0)
-##    node2 = MyNode('RLEAF', '3', 1)
-##    node3 = MyNode('LEAF', '2', 2)
-##    node4 = MyNode('RLEAF', '3', 3)
-##    node5 = MyNode('LNODE', '*', 4)
-##    node6 = MyNode('RNODE', '*', 5)
-##    node7 = MyNode('ROOT', '+', 6)
-##
-##    nodes = [node1, node2, node3, node4, node5, node6, node7]
-##    
-##    for node in nodes:
-##        print(node)
+    # With two leafs
+    #node = MyNode('+', 1, 2)
 
-
-    #node1 = MyNode('+', 2, 2)
-    node1 = MyNode('+', 2)
-    print(node1)
+    # With only one leaf
+    #node = MyNode('+', 2)
+    
+    print(node)
