cfa5ecd4582a53740a7e76d2f7c46674f2badfc2
Sonia Goncalves <i_code@fastmail.org>
2021-09-11 00:08:26 +0100

    Docstring, and __str__() method.

--- myEvaluator.py
+++ myEvaluator.py
@@ -21,8 +21,26 @@ class MyEvaluator:
         self._node = node
         self._result = 0
 
-        
-    #def __str__(self):
+
+    def __str__(self):
+        '''(MyEvaluator) -> str
+
+        Return a string representation of the parser.
+
+        >>> node = MyNode('+', 1, 2)
+        >>> eva = MyEvaluator(node)
+        >>> result = eva.getResult()
+        >>> print(eva)
+        (+ 1 2) = 3
+        '''
+
+        if self._node.getRightOperand() != None:
+            return '(' + self._node.getOperator() + ' ' + str(self._node.getLeftOperand()) + ' ' + \
+                   str(self._node.getRightOperand()) + ') = ' + str(self._result)
+
+        else:
+            return '(' + self._node.getOperator() + ' ' + str(self._node.getLeftOperand()) + ') = ' + \
+                   str(self._result)
         
 
     # Docstring?
@@ -60,10 +78,18 @@ class MyEvaluator:
                 self._result = self._node.getLeftOperand() / self._node.getRightOperand()
 
 
-    # Docstring?
     def getResult(self):
-        '''(MyEvaluator) -> int'''
+        '''(MyEvaluator) -> int
 
+        Return the result of the evaluation of the expression.
+
+        >>> node = MyNode('+', 1, 2)
+        >>> eva = MyEvaluator(node)
+        >>> result = eva.getResult()
+        >>> print(result)
+        3
+        '''
+        
         self._performOperation()
 
         return self._result
@@ -75,3 +101,13 @@ if __name__ == '__main__':
     # Testing automatically using doctest module.
     import doctest
     doctest.testmod()
+
+    # Creating some examples:
+    #node = MyNode('+', 11)
+    node = MyNode('+', 1, 2)
+    
+    eva = MyEvaluator(node)
+    result = eva.getResult()
+    print(result)
+
+    print(eva)
