02464955e8f7fd467aa3cfb39590d205e56de69b
Sonia Goncalves <i_code@fastmail.org>
2021-09-25 23:40:46 +0100

    Added more unit tests.

--- test_myTokenizer.py
+++ test_myTokenizer.py
@@ -60,6 +60,41 @@ class TestMyTokenizer(unittest.TestCase):
         expected = "'+ 1 2' -> Tokenizer -> ['+', '1', '2']"
 
         self.assertEqual(actual, expected)
+
+
+    def test_getListOfTokens_parenthesesIncluded(self):
+
+        tkz = MyTokenizer('+ 2 (* 2 2)')
+        lisTokens = tkz.getListOfTokens()
+
+        actualValueTokens = []
+        for i in range(len(lisTokens)):
+            actualValueTokens.append(lisTokens[i].getValue())
+             
+        expectedValueTokens = ['+', '2', '(', '*', '2', '2', ')']
+        
+        self.assertEqual(actualValueTokens, expectedValueTokens)
+
+
+    def test__creaTokens_parenthesesIncluded(self):
+
+        tkz = MyTokenizer('+ 2 (* 2 2)')
+        actuaLisTokens = tkz.getListOfTokens()
+
+        expectedLisTokens = tkz._output 
+
+        self.assertEqual(actuaLisTokens, expectedLisTokens)
+
+
+    def test_str_parenthesesIncluded(self):
+         
+        tkz = MyTokenizer('+ 2 (* 2 2)')
+        tkz.getListOfTokens()
+         
+        actual = str(tkz)
+        expected = "'+ 2 (* 2 2)' -> Tokenizer -> ['+', '2', '(', '*', '2', '2', ')']"
+
+        self.assertEqual(actual, expected)
         
 
 if __name__ == '__main__':
