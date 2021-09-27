6afe80696871414e836ad1ceb190bc60164f99e3
Sonia Goncalves <i_code@fastmail.org>
2021-09-28 00:31:27 +0100

    Review before submission. Added more unit tests.

--- test_myParser.py
+++ test_myParser.py
@@ -1,7 +1,7 @@
 # Name: Sonia Gonçalves
-# Program: IT MSc
-# ST ID: 
-# Date: 19 09 21
+# Program: MSc IT
+# ST ID: 13106604
+# Date: September 2021
 
 
 ##
@@ -12,11 +12,11 @@ import unittest
 from myParser import MyParser
 from myTokenizer import MyTokenizer
 
+
 class TestMyParser(unittest.TestCase):
     '''Test class for MyParser class.'''
     
-
-    def test_getResult1(self):
+    def test_getResult(self):
 
         tkz = MyTokenizer('+ 1 2')
         tokens = tkz.getListOfTokens()
@@ -28,20 +28,18 @@ class TestMyParser(unittest.TestCase):
         self.assertEqual(actualResult, expectedResult)
 
 
-    def test_getResult2(self):
+    def test_getResult_valueInstanceVariable(self):
 
         tkz = MyTokenizer('+ 1 2')
         tokens = tkz.getListOfTokens()
         psr = MyParser(tokens)
 
+        # private instance variable (should use accessor, but it is not implemented)
         actualResult = psr._result
         psr.getResult()
         expectedResult = psr._result
         
         self.assertNotEqual(actualResult, expectedResult)
-
-
-    # Tests _getExpression, _getTerm, _getFactor !?
 
 
     def test_str(self):
@@ -54,6 +52,84 @@ class TestMyParser(unittest.TestCase):
          
         actual = str(psr)
         expected = "[] -> Parser -> [['+', 1, 2]]"
+
+        self.assertEqual(actual, expected)
+
+
+    def test_getResult_withParentheses(self):
+
+        tkz = MyTokenizer('+ (* 2 2) (* 2 3)')
+        tokens = tkz.getListOfTokens()
+        psr = MyParser(tokens)
+
+        actualResult = psr.getResult()
+        expectedResult = 10
+        
+        self.assertEqual(actualResult, expectedResult)
+
+
+    def test_getResult_valueInstanceVariable_withParentheses(self):
+
+        tkz = MyTokenizer('+ (* 2 2) (* 2 3)')
+        tokens = tkz.getListOfTokens()
+        psr = MyParser(tokens)
+
+        actualResult = psr._result
+        psr.getResult()
+        expectedResult = psr._result
+        
+        self.assertNotEqual(actualResult, expectedResult)
+
+
+    def test_str_withParentheses(self):
+         
+        tkz = MyTokenizer('+ (* 2 2) (* 2 3)')
+        tokens = tkz.getListOfTokens()
+        
+        psr = MyParser(tokens)
+        psr.getResult()
+         
+        actual = str(psr)
+        expected = "[] -> Parser -> [['*', 2, 2], ['*', 2, 3], ['+', 4, 6]]"
+
+        self.assertEqual(actual, expected)
+
+
+    def test_getResult_nestedParentheses(self):
+
+        tkz = MyTokenizer('+ (+ 2 3)(+ (* 2 1) (* 2 2))')
+        tokens = tkz.getListOfTokens()
+        psr = MyParser(tokens)
+
+        actualResult = psr.getResult()
+        expectedResult = 11
+        
+        self.assertEqual(actualResult, expectedResult)
+
+
+    def test_getResult_valueInstanceVariable_nestedParentheses(self):
+
+        tkz = MyTokenizer('+ (+ 2 3)(+ (* 2 1) (* 2 2))')
+        tokens = tkz.getListOfTokens()
+        psr = MyParser(tokens)
+
+        actualResult = psr._result
+        psr.getResult()
+        expectedResult = psr._result
+        
+        self.assertNotEqual(actualResult, expectedResult)
+
+
+    def test_str_nestedParentheses(self):
+         
+        tkz = MyTokenizer('+ (+ 2 3)(+ (* 2 1) (* 2 2))')
+        tokens = tkz.getListOfTokens()
+        
+        psr = MyParser(tokens)
+        psr.getResult()
+         
+        actual = str(psr)
+        expected = "[] -> Parser -> [['+', 2, 3], ['*', 2, 1], ['*', 2, 2], ['+', 2, 4], ['+', 5, 6]]"
 
         self.assertEqual(actual, expected)
         
